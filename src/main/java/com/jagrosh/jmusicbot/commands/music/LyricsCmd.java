bed46d0a92cffe2064348229f9a58b19fb5f9dfc
Assasinowy <110182021+Assasinowy@users.noreply.github.com>
2024-11-28 17:53:04 +0100

    Update LyricsCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/music/LyricsCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/LyricsCmd.java
@@ -35,8 +35,8 @@ public class LyricsCmd extends MusicCommand
     {
         super(bot);
         this.name = "lyrics";
-        this.arguments = "[song name]";
-        this.help = "shows the lyrics of a song";
+        this.arguments = "[nazwa piosenki/song name]";
+        this.help = "[PL] Pokazuje tekst danej piosenki [ENG] shows the lyrics of a song";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS};
     }
@@ -52,7 +52,7 @@ public class LyricsCmd extends MusicCommand
                 title = sendingHandler.getPlayer().getPlayingTrack().getInfo().title;
             else
             {
-                event.replyError("There must be music playing to use that!");
+                event.replyError("[PL] Musi byc wlaczona jakas muzyczka aby uzyc tej komendy! [ENG] There must be music playing to use that!");
                 return;
             }
         }
@@ -63,7 +63,7 @@ public class LyricsCmd extends MusicCommand
         {
             if(lyrics == null)
             {
-                event.replyError("Lyrics for `" + title + "` could not be found!" + (event.getArgs().isEmpty() ? " Try entering the song name manually (`lyrics [song name]`)" : ""));
+                event.replyError("[PL] Tekst `" + title + "` nie zostal znaleziony!" + (event.getArgs().isEmpty() ? " Sprobuj wpisac nazwe piosenki (`lyrics [nazwa piosenki]`) [ENG] Lyrics for `" + title + "` could not be found!" + (event.getArgs().isEmpty() ? " Try entering the song name manually (`lyrics [song name]`)" : ""));
                 return;
             }
 
@@ -73,7 +73,7 @@ public class LyricsCmd extends MusicCommand
                     .setTitle(lyrics.getTitle(), lyrics.getURL());
             if(lyrics.getContent().length()>15000)
             {
-                event.replyWarning("Lyrics for `" + title + "` found but likely not correct: " + lyrics.getURL());
+                event.replyWarning("[PL] Tekst `" + title + "` zostal znaleziony ale raczej nie jest poprawny [ENG] Lyrics for `" + title + "` found but likely not correct: " + lyrics.getURL());
             }
             else if(lyrics.getContent().length()>2000)
             {
