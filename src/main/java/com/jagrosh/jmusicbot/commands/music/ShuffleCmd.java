6bf17df2155f7f7ed74bcc2bec7de6e113b0f195
Assasinowy <110182021+Assasinowy@users.noreply.github.com>
2024-11-28 20:03:34 +0100

    Update ShuffleCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/music/ShuffleCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/ShuffleCmd.java
@@ -30,7 +30,7 @@ public class ShuffleCmd extends MusicCommand
     {
         super(bot);
         this.name = "shuffle";
-        this.help = "shuffles songs you have added";
+        this.help = "miesza utwory ktore dodales";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.beListening = true;
         this.bePlaying = true;
@@ -44,13 +44,13 @@ public class ShuffleCmd extends MusicCommand
         switch (s) 
         {
             case 0:
-                event.replyError("You don't have any music in the queue to shuffle!");
+                event.replyError("Nie masz zadnych utworow z kolejce do pomieszania!");
                 break;
             case 1:
-                event.replyWarning("You only have one song in the queue!");
+                event.replyWarning("Masz tylko 1 utwor!");
                 break;
             default:
-                event.replySuccess("You successfully shuffled your "+s+" entries.");
+                event.replySuccess("Pomyslnie pomieszales **"+s+"** utworow/utwory.");
                 break;
         }
     }
